b6fd2884a916a996fea02158f20b5cb4d1e50e1e
johnwu94 <johnwu94@hotmail.com>
2021-09-01 19:35:34 +1000

    correcto8

--- 00_framework/cp-jar.sh
+++ 00_framework/cp-jar.sh
@@ -1,17 +1,3 @@
-# #!/bin/bash
-# mkdir ../alltxt
-# touch list.txt
-
-# for i in ./*; do 
-#     #echo "$i"
-#     if [ -d "$i" ]; then
-#         echo "$i is a dir" ;
-#         cp $i/*.txt ../alltxt/       
-#     else
-#         echo "$i is not a dir"
-#     fi
-# done
-
 #!/bin/bash
 mkdir ../alljar
 
